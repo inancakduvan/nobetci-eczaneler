c514833942f2ea6fa1f24bd9d1e3f0cabc7bf72b
İnanç Akduvan <49209002+inancakduvan@users.noreply.github.com>
2024-06-09 12:55:35 +0300

    time logic of pharmacies cache updated

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,12 +1,13 @@
 import { StorageKeys } from "@/enums";
 
 export const setDataOfTodaysPharmacies = (value: string) => {
-    const date = new Date().setHours(7,0,0);
+    const currentDate = new Date().setHours(7,0,0);
+    const tomorrow = currentDate + (60 * 60 * 24 * 1000);
 
     const valueParsed = JSON.parse(value);
 
     const newData = {
-        date,
+        date: tomorrow,
         value: valueParsed
     }
 
