c7b02942141fa581e0c0f0424751016627a7d379
İnanç Akduvan <49209002+inancakduvan@users.noreply.github.com>
2024-06-13 00:36:59 +0300

    component reset funcs improved

--- components/SelectState.tsx
+++ components/SelectState.tsx
@@ -97,6 +97,7 @@ const SelectState: TSelectState = ({stateType}) => {
 
         return () => {
             setSearchedResultList([]);
+            setIsResultsLoading(false);
         }
     }, [])
 
