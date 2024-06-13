f30fac1218e9e3c4569040ce7a0db4bc480bfeaf
İnanç Akduvan <49209002+inancakduvan@users.noreply.github.com>
2024-06-14 01:49:30 +0300

    structure of list cities updated

--- components/SelectState.tsx
+++ components/SelectState.tsx
@@ -157,18 +157,19 @@ const SelectState: TSelectState = ({stateType}) => {
                 isComponentLoaded ?
                 <div className={"relative mt-medium px-medium pb-medium"}>
                     {searchedResultList && searchedResultList.length > 0 ? searchedResultList.map((state, index) => 
-                    <motion.div 
-                        custom={index}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ delay: 0.04 * index }}
-                        key={"city-" + state}
-                        onClick={() => setCityAndDistrict(state)}>
-                        <div className="flex items-center justify-between px-medium text-subheading-medium h-[60px] border-b border-solid border-muted-700 cursor-pointer">
-                            {state.toLocaleUpperCase('tr-TR')} 
-                            {(isResultsLoading && state === selectedCity) && <Spinner />}
-                        </div>
-                    </motion.div>)
+                    <div onClick={() => setCityAndDistrict(state)}>
+                        <motion.div 
+                            custom={index}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 0.04 * index }}
+                            key={"city-" + state}>
+                            <div className="flex items-center justify-between px-medium text-subheading-medium h-[60px] border-b border-solid border-muted-700 cursor-pointer">
+                                {state.toLocaleUpperCase('tr-TR')} 
+                                {(isResultsLoading && state === selectedCity) && <Spinner />}
+                            </div>
+                        </motion.div>
+                    </div>)
                     :
                     null
                     }
